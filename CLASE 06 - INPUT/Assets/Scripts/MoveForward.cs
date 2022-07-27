16637233cbe9f8ecd6ae46d49fec203c9024c55e
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-07-27 09:33:14 -0300

    Code Live
    Player Move Script
    MovePlayer Method
    Shoot Method

--- CLASE 06 - INPUT/Assets/Scripts/MoveForward.cs
+++ CLASE 06 - INPUT/Assets/Scripts/MoveForward.cs
@@ -6,9 +6,11 @@ public class MoveForward : MonoBehaviour
 {
     public float speed = 2f;
 
+    public float liveTime = 3f;
+
     void Start()
     {
-
+        Invoke("DestroyDelay", liveTime);
     }
 
     // Update is called once per frame
@@ -20,6 +22,13 @@ public class MoveForward : MonoBehaviour
     //Método para mover el trasform usando la posicion.
     private void Move()
     {
-        transform.position += Vector3.forward * speed * Time.deltaTime;
+        //transform.position += Vector3.forward * speed * Time.deltaTime;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void DestroyDelay()
+    {
+        Debug.Log("DESTROY DELAY");
+        Destroy(gameObject);
     }
 }
---
+++ CLASE 06 - INPUT/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 3f;
+    // Start is called before the first frame update
+    public GameObject munition;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            MovePlayer(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            MovePlayer(Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MovePlayer(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MovePlayer(Vector3.right);
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Shoot();
+        }
+
+    }
+
+    private void MovePlayer(Vector3 direction)
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void Shoot()
+    {   
+        Debug.Log("DISPARAR");
+        Instantiate(munition, transform.position, transform.rotation);
+    }
+}
